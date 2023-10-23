8e406cbb71640a172fbfceadad4ccdbf1e9a86fa
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-10-23 22:49:41 +0300

    refactor make_numbers function

--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 
+from masks import mask_card_number, mask_account_number
+
 
 def make_numbers(data: str) -> str:
-    """Приводит номер карты/счета в замаскированный вид"""
-    if "Счет" in data:
-        return f"{data[:5]}**{data[-4:]}"
+    """Получает на вход строку и возвращает номер карты/счета с замаскированным значением"""
+    data_split = data.split(" ")
+    name = " ".join(data_split[:-1])
+    if len(data_split[-1]) == 16:
+        masked_number = mask_card_number(data_split[-1])
     else:
-        return f"{data[:-16]}{data[-16:-12]} {data[-12:-10]}** **** {data[-4:]}"
+        masked_number = mask_account_number(data_split[-1])
+    return f"{name} {masked_number}"
 
 
 def fix_date(date: str) -> str:
