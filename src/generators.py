9931389f1349fcf5ad998597f0db66542bbb41f2
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-10-30 20:58:00 +0300

    add 3 funcs with generators

---
+++ src/generators.py
@@ -0,0 +1,27 @@
+from typing import Iterable, Any
+
+
+def filter_by_currency(data: list[dict], currency: str) -> Iterable[dict[str, Any]]:
+    """
+    Принимает список словарей data и валюту currency.
+    Возвращает итератор, выдающий операции с указанной валютой.
+    """
+    return (x for x in data if x["operationAmount"]["currency"]["code"] == currency)
+
+
+def transaction_descriptions(transactions: list[dict]) -> Iterable[str]:
+    """
+    Принимает список словарей transactions.
+    Возвращает итератор, выдающий описания операций из словарей.
+    """
+    return (x["description"] for x in transactions)
+
+
+def card_number_generator(start: int, end: int) -> Iterable[str]:
+    """
+    Принимает начальное и конечное числа start и end.
+    Генерирует номера карт в указанном диапазоне и возвращает итератор.
+    """
+    for num in range(start, end + 1):
+        number = f"{'0' * (16 - len(str(num)))}{num}"
+        yield f"{number[:4]} {number[4:8]} {number[8:12]} {number[12:]}"
