28bb44b5bddd8e33729e5fa0f8a4270550dd1f6d
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-06 22:10:21 +0300

    refactor log function to use prints

--- src/decorators.py
+++ src/decorators.py
@@ -9,27 +9,26 @@ def log(*, filename: str | None = None) -> Callable:
 
     def wrapper(func: Callable) -> Callable:
         @wraps(func)
-        def inner(*args: Optional[Any], **kwargs: Optional[Any]) -> Optional[Any]:
+        def inner(*args: Optional[Any], **kwargs: Optional[Any]) -> None:
             time = str(datetime.now())
             if not filename:
                 try:
                     result = func(*args, **kwargs)
                     text = f"{time[:-7]} {func.__name__} ok\n"
-                    return f"{result}\n{text}"
+                    print(f"{result}\n{text}")
                 except Exception:
                     err_type = sys.exc_info()
-                    return f"{time[:-7]} {func.__name__} error {err_type[1]}. Inputs: {args}, {kwargs}\n"
+                    print(f"{time[:-7]} {func.__name__} error {err_type[1]}. Inputs: {args}, {kwargs}\n")
             else:
                 with open(filename, "a+") as file:
                     try:
                         result = func(*args, **kwargs)
                         text = f"{time[:-7]} {func.__name__} ok\n"
                         file.write(text)
-                        return result
+                        print(result)
                     except Exception:
                         err_type = sys.exc_info()
                         file.write(
                             f"{time[:-7]} {func.__name__} error {err_type[1]}. Inputs: {args}, {kwargs}\n")
-                        return None
         return inner
     return wrapper
