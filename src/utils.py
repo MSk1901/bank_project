8eecc56563ac850c9999cd01111a4726704924fd
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-10 21:19:48 +0300

    refactor functions so mypy shows no errors

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import json
 import os
+from typing import Any
+
 import requests
 from dotenv import load_dotenv
 
 
-def get_data(filename: str) -> list[dict]:
+def get_data(filename: str) -> Any:
     """Получает данные из json файла если он существует и в нем хранятся корректные данные"""
     try:
         with open(filename) as file:
@@ -13,13 +15,13 @@ def get_data(filename: str) -> list[dict]:
                 return []
             else:
                 with open(filename) as f:
-                    data = json.load(f)
-                    return data
+                    data_json = json.load(f)
+                    return data_json
     except FileNotFoundError:
         return []
 
 
-def transaction_amount(transaction: dict) -> float | str:
+def transaction_amount(transaction: dict) -> float:
     """Возвращает сумму транзакции в рублях,
     при необходимости конвертируя другую валюту в рубли"""
 
@@ -27,17 +29,18 @@ def transaction_amount(transaction: dict) -> float | str:
     if currency == "RUB":
         return float(transaction["operationAmount"]["amount"])
     elif currency == "USD" or currency == "EUR":
+        load_dotenv()
+        api_key = os.getenv("EXCHANGE_RATE_API_KEY")
+        if api_key is None:
+            raise ValueError("Нет ключа API")
         try:
-            load_dotenv()
-            API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
-
             url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency}"
-            response = requests.get(url, headers={'apikey': API_KEY})
+            response = requests.get(url, headers={'apikey': api_key})
             response.raise_for_status()
             response_data = response.json()
             rate = response_data["rates"]["RUB"]
 
-            amount = float(transaction["operationAmount"]["amount"]) * rate
+            amount: float = float(transaction["operationAmount"]["amount"]) * rate
             return round(amount, 2)
         except (requests.exceptions.HTTPError, ValueError, KeyError):
             raise ValueError("Что-то пошло не так")
