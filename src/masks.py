c54bc66568ea17713b8f25793e440a3d78fa0656
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-15 19:28:43 +0300

    add logging to funcs from masks.py & utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,26 @@
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("masks.log", "w+", "utf-8")
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def mask_card_number(number: str) -> str:
     """Приводит номер карты в замаскированный вид"""
     if len(number) == 16:
+        logger.debug("Передан корректный номер карты")
         return f"{number[:4]} {number[4:6]}** **** {number[12:]}"
-    return "Некорректный номер карты"
+    logger.error("Передан некорректный номер карты")
+    raise ValueError("Некорректный номер карты")
 
 
 def mask_account_number(number: str) -> str:
     """Приводит номер счета в замаскированный вид"""
     if len(number) == 20:
+        logger.debug("Передан корректный номер счета")
         return f"**{number[-4:]}"
-    return "Некорректный номер счета"
+    logger.error("Передан некорректный номер счета")
+    raise ValueError("Некорректный номер счета")
